58e0b0ba4d68e2df4a7c0dbef663db78883f2bc9
Aidar <nasibullin.aid@gmail.com>
2020-05-16 13:18:10 +0500

    feat: add jquery,  make code for modal on jquery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
+//  document.addEventListener("DOMContentLoaded", function (event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const modalClose = document.querySelector('[data-toggle=close]');
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const modalClose = document.querySelector('[data-toggle=close]');
+//   modalBtn.forEach(function (element) {
+//     element.addEventListener('click', function () {
+//       modal.classList.toggle('modal--visible');
 
-  modalBtn.forEach(function (element) {
-    element.addEventListener('click', function () {
-      modal.classList.toggle('modal--visible');
+//     });
 
-    });
-  
+//   });
+//   modalClose.addEventListener('click', function () {
+//     modal.classList.toggle('modal--visible')
+//   });
+// });
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closeBtn = $('.modal__close');
+
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
   });
-  modalClose.addEventListener('click', function () {
-    modal.classList.toggle('modal--visible')
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
   });
+
+
 });
